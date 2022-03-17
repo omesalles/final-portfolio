36f93b5763d2c760aa9f8e457c2bf213f97b2ddb
omesalles <omesalles@edu.tecnocampus.cat>
2022-03-17 23:25:02 +0100

    Update Welcome.js
    changing placeholder for side navigator

--- src/pages/components/Welcome/Welcome.js
+++ src/pages/components/Welcome/Welcome.js
@@ -4,7 +4,7 @@ export function Welcome() {
   let component = "Welcome";
   let title = "Welcome";
   let panellLeft = [1, 2, 3];
-  let panellRight = [1, 2, 3];
+  let panellRight = [1, 2];
   let panellMiddle = ["one", "two", "three"];
   return (
     <Panell
